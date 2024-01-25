604b3cfce73eae65cd685c52c31e1db2a738d245
Ju-MINJAE <juminjae98@gmail.com>
2024-01-25 19:40:46 +0900

    Update 5.promise-egg.js

--- 13.promise/5.promise-egg.js
+++ 13.promise/5.promise-egg.js
@@ -1,3 +1,4 @@
+// Promise.resolve() - 주어진 값으로 이행하는 Promise 객체를 반환
 function fetchEgg(chicken) {
   return Promise.resolve(`${chicken}=>🥚`);
 }
@@ -6,9 +7,10 @@ function fryEgg(egg) {
   return Promise.resolve(`${egg}=>🍳`);
 }
 
+// Promise.reject(reason) - 주어진 사유로 거부하는 Promise 객체를 반환
 function getChichen() {
-  return Promise.reject(new Error('치킨을 가지고 올 수 없음!'));
-  //return Promise.resolve(`🪴=>🐓`);
+  //return Promise.reject(new Error('치킨을 가지고 올 수 없음!'));
+  return Promise.resolve(`🪴=>🐓`);
 }
 
 getChichen()
