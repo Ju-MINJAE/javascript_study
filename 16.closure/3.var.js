e6b22f3361e4a803e93d01487097f70ce4e0a468
Ju-MINJAE <juminjae98@gmail.com>
2024-09-24 18:27:46 +0900

    Update 3.var.js

--- 16.closure/3.var.js
+++ 16.closure/3.var.js
@@ -1,6 +1,9 @@
 function loop() {
   const array = [];
   //var과 let의 차이점!
+  // let 사용 >> 0,1,2,3,4
+  // var 사용 >> 5,5,5,5,5
+  // var는 블록스코프가 존재하지 않음. 함수스코프만 존재.
   for (let i = 0; i < 5; i++) {
     array.push(function () {
       console.log(i);
